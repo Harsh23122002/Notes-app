bc4103063f63a15d94cc1d80ca9e239e20cbcafb
Harsh23122002 <86722997+Harsh23122002@users.noreply.github.com>
2022-07-23 16:52:57 +0530

    Basic read from note implemented

--- lib/pages/newnote.dart
+++ lib/pages/newnote.dart
@@ -1,4 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+final db = FirebaseFirestore.instance;
+
+// Create a new user with a first and last name
+final notes = <String, dynamic>{
+  "first": "Ada",
+  "last": "Lovelace",
+  "born": 1815
+};
+
 
 class NewNote extends StatefulWidget {
   const NewNote({Key? key}) : super(key: key);
@@ -8,14 +19,59 @@ class NewNote extends StatefulWidget {
 }
 
 class _NewNoteState extends State<NewNote> {
+  late String title;
+  late String body;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0x6FFFFF6F),
+        actions: [
+          TextButton(
+              onPressed: () {
+                print("Text : $title\nBody : $body");
+
+                // Add a new document with a generated ID
+                db.collection("notes").add({
+                  "title" : title,
+                  "desc" : body
+                }).then((DocumentReference doc) =>
+                    print('DocumentSnapshot added with ID: ${doc.id}'));
+
+              },
+              child: const Text("Save"))
+        ],
+      ),
       body: SafeArea(
         child: Column(
           children: [
-            TextField(),
-            TextFormField()
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (text) {
+                  title = text;
+                },
+                style: const TextStyle(fontSize: 30),
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Title"),
+              ),
+            ),
+            const SizedBox(height: 0),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  maxLines: 26,
+                  onChanged: (text) {
+                    body = text;
+                  },
+                  style: const TextStyle(fontSize: 20),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Title"),
+                ),
+              ),
+            )
           ],
         ),
       ),
